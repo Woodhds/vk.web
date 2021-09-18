ac686dcb5428e97a3f7f8c64ccc3c22a44240634
s.borodin <woodhds@gmail.com>
2021-09-18 19:06:58 +0300

    disable theme

--- src/pages/users/add.vue
+++ src/pages/users/add.vue
@@ -2,7 +2,9 @@
   <form class="flex flex-col w-2/5 md:w-1/5 gap-3 mb-3" @submit.prevent="makeSearch">
     <label class="flex flex-col">
       Поиск
-      <input v-model="search" autocomplete="off" autofocus required class="border border-blue-500 focus:outline-none focus:ring" />
+      <input v-model="search" autocomplete="off" autofocus required
+             class="border border-blue-500 focus:outline-none focus:ring"
+      />
     </label>
     <button type="submit" class="btn w-1/2">
       Отправить
@@ -10,10 +12,12 @@
   </form>
   <div class="flex flex-col gap-3">
     <div v-for="user in users" :key="user.id" class="flex flex-row gap-x-3 items-center gep-y-3">
-      <div class="h-12 w-12 rounded-full bg-center bg-no-repeat bg-cover" :style="{ background: `url(${user.avatar})` }"></div>
+      <div class="h-12 w-12 rounded-full bg-center bg-no-repeat bg-cover"
+           :style="{ background: `url(${user.avatar})` }"
+      ></div>
       <a :href="`https://vk.com/id${user.id}`" target="_blank" class="w-1/5">{{ user.name }}</a>
       <button class="btn" @click="add(user)">
-        <carbon-add />
+        <carbon-add/>
       </button>
     </div>
   </div>
@@ -35,6 +39,6 @@ const makeSearch = async() => {
 }
 
 const add = async(user: VkUser) => {
-  store.dispatch(`user/${ActionTypes.add}`, user)
+  await store.dispatch(`user/${ActionTypes.add}`, user)
 }
 </script>
