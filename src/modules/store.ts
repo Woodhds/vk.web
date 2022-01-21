6407378832bef4e67464c15ce4385b297e565644
sborodin <serborodin@ozon.ru>
2022-01-21 10:21:50 +0300

    fix build

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,6 +1,13 @@
 import {createPinia} from "pinia";
-import type { UserModule } from '~/types'
+import type {UserModule} from '~/types'
 
-export const install: UserModule = ({ app }) => {
-  app.use(createPinia())
+export const install: UserModule = ({app, initialState}) => {
+  const pinia = createPinia()
+  app.use(pinia)
+
+  if (import.meta.env.SSR) {
+    initialState.pinia = pinia.state.value
+  } else {
+    pinia.state.value = initialState.pinia || {}
+  }
 }
