9e77b0d5df11777149d64d06db40a9509d362af8
sborodin <serborodin@ozon.ru>
2022-02-04 12:44:40 +0300

    get by id

--- src/pages/predictions/index.vue
+++ src/pages/predictions/index.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="mt-3">
-    <form class="flex flex-col w-1/2" @submit.prevent="submit">
-      <label>OwnerId</label>
-      <input v-model="model.ownerId" class="border border-blue-500 focus:outline-none focus:ring mb-3" type="number"/>
-      <label>Id</label>
-      <input v-model="model.id" class="border border-blue-500 focus:outline-none focus:ring mb-3" type="number"/>
-      <label for="text">Текст</label>
-      <textarea
-        id="text"
-        v-model="model.text"
-        rows="7"
-        class="border border-blue-500 focus:outline-none focus:ring mb-3"
+    <form class="flex flex-col md:w-1/2 w-full" @submit.prevent="submit">
+      <label for="message-id">Id</label>
+      <input
+        id="message-id"
+        v-model="textId"
+        class="border border-blue-500 focus:outline-none focus:ring"
+        :class="{
+          'mb-3': !warning
+        }"
+      />
+      <span v-if="warning" class="text-red-500 mb-3">{{ warning }}</span>
+      <pre
+        class="
+          leading-4
+          whitespace-pre-wrap
+          h-48
+          overflow-auto
+          text-xs
+          min-h-64
+          border border-blue-500
+          mb-3
+        "
+        v-html="text"
       />
       <div class="flex mb-3">
         <button class="btn" type="submit">Отправить</button>
       </div>
     </form>
     <div class="flex-row flex">
-      <ul class="flex flex-row w-full justify-between">
-        <li v-for="(k, v) in predictions" :key="k" class="flex flex-col">
+      <ul class="flex flex-row w-full justify-between flex-wrap">
+        <li v-for="(k, v) in predictions" :key="k" class="flex flex-col w-1/5 p-1">
           <div>{{ v }}</div>
           <div>{{ Math.round(k * 100) }}%</div>
         </li>
@@ -29,27 +41,36 @@
 
 <script lang="ts" setup>
 import messageService from "~/services/messages";
-import type {PredictResult} from "~/services/types";
+import type { PredictResult } from "~/services/types";
 
-const model = reactive({
-  text: '',
-  ownerId: '',
-  id: ''
-});
-const predictions = ref<PredictResult>({})
+const regexp = /\d+_\d+/;
+
+const textId = ref("");
+const text = ref("");
+const warning = ref("");
+const predictions = ref<PredictResult>({});
+
+const validate = () => {
+  if (!regexp.test(textId.value)) {
+    warning.value = "Неверный идентификатор";
+  } else {
+    warning.value = "";
+  }
+};
 
 const submit = async () => {
-  const data = await messageService.predict(+model.ownerId, +model.id, model.text);
-  const keys = Object.keys(data).map(d => ({ key: d, value: data[d] }))
+  validate();
+  if (warning.value) return;
+
+  const [ownerId, id] = textId.value.split("_");
+
+  const { predict, message } = await messageService.predict(+ownerId, +id);
+  text.value = message?.text;
+  const keys = Object.keys(predict).map((d) => ({ key: d, value: predict[d] }));
   keys.sort((a, b) => b.value - a.value);
   predictions.value = keys.reduce((obj, key) => {
-    obj[key.key] = data[key.key]
+    obj[key.key] = predict[key.key];
     return obj;
-  }, {} as PredictResult)
-}
-
+  }, {} as PredictResult);
+};
 </script>
-
-<style scoped>
-
-</style>
--- src/services/messages.ts
+++ src/services/messages.ts
@@ -1,37 +1,37 @@
-import ApiClient from './apiClient'
-import {PredictResult} from "./types";
-import type {VkMessage} from './types'
+import ApiClient from "./apiClient";
+import type { PredictResult, VkMessage } from "./types";
 
 class MessageService {
-  async getMessages(search = '') {
-    return await ApiClient.getAsync<VkMessage[] | null>('/messages', {params: {search}})
+  async getMessages(search = "") {
+    return await ApiClient.getAsync<VkMessage[] | null>("/messages", {
+      params: { search },
+    });
   }
 
   async repost(messages: { id: number; owner_Id: number }[]) {
-    return await ApiClient.postAsync('/repost', messages)
+    return await ApiClient.postAsync("/repost", messages);
   }
 
   async save(ownerId: number, id: number, category: string) {
-    return await ApiClient.postAsync(`messages/${ownerId}/${id}`, {category})
+    return await ApiClient.postAsync(`messages/${ownerId}/${id}`, { category });
   }
 
   async like() {
-    return await ApiClient.postAsync('/like')
+    return await ApiClient.postAsync("/like");
   }
 
   async grab() {
-    return await ApiClient.getAsync("/grab")
+    return await ApiClient.getAsync("/grab");
   }
 
-  async predict(ownerId: number, id: number, text: string) {
-    return await ApiClient.postAsync<PredictResult>(`predict/${ownerId}/${id}`, text, {
-      headers: {
-        "Content-type": "application/json"
-      }
-    });
+  async predict(ownerId: number, id: number) {
+    return await ApiClient.postAsync<{
+      message: VkMessage;
+      predict: PredictResult;
+    }>(`predict/${ownerId}/${id}`);
   }
 }
 
-const messageService = new MessageService()
+const messageService = new MessageService();
 
 export default messageService
