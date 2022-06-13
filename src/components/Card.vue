7939f4d389bd82e2aea50499602952f064d41f5d
s.borodin <woodhds@gmail.com>
2022-06-13 17:05:42 +0300

    text parse

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
-import type {VkMessage} from "~/api/types";
+import type { VkMessage } from '~/api/types'
 
-defineProps({
+const regexp = /\[(club[0-9]+)\|(.+)\]/
+
+const props = defineProps({
   message: {
     type: Object as () => VkMessage,
     default: () => null,
-    required: true
-  }
+    required: true,
+  },
+})
+
+const text = computed(() => {
+  return props.message.text.replace(regexp, '<a href=\'https://vk.com/$1\' target=\'_blank\' style=\'color: #2a5885;font-weight: 500\'>$2</a>')
 })
 </script>
 
@@ -18,14 +24,14 @@ defineProps({
       class="block flex-1 text-xl truncate"
       target="_blank"
     >
-      {{ message.owner ? message.owner : "Пост" }}
+      {{ message.owner ? message.owner : 'Пост' }}
     </a>
   </div>
 
-  <card-image :src="message.images" />
+  <card-image :src="message.images"/>
   <pre
     class="leading-4 whitespace-pre-wrap h-48 overflow-auto text-xs"
-    v-html="message.text"
+    v-html="text"
   ></pre>
   <slot :message="message" name="bottom"></slot>
 </template>
