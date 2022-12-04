5ffe4e1a2d0f6b2bb9c6c5d262921158073e6505
s.borodin <woodhds@gmail.com>
2022-12-04 12:37:05 +0300

    show date

--- src/components/CardPanel.vue
+++ src/components/CardPanel.vue
@@ -7,28 +7,33 @@
         target="_blank"
         flat
         dense
+        padding="0"
         class="card__title"
       >
         {{ message.owner ? message.owner : 'Пост' }}
       </q-btn>
+      <q-item-label caption>
+        {{ date.formatDate(message.date, 'DD.MM.YYYY HH:mm:ss') }}
+      </q-item-label>
     </q-card-section>
     <q-card-section>
-      <card-image :src="message.images" />
+      <card-image :src="message.images"/>
     </q-card-section>
     <q-card-section>
       <q-scroll-area style="height: 12rem; width: 100%">
         <pre class="card__text" v-html="text"></pre>
       </q-scroll-area>
     </q-card-section>
-    <q-separator />
+    <q-separator/>
     <slot :message="message" name="bottom"></slot>
   </q-card>
 </template>
 
 <script lang="ts" setup>
-import type { VkMessage } from 'src/api/types';
-import { computed } from 'vue';
+import type {VkMessage} from 'src/api/types';
+import {computed} from 'vue';
 import CardImage from 'src/components/CardImage.vue';
+import {date} from 'quasar'
 
 const regexp = /\[(club[0-9]+)\|(.+)\]/;
 
@@ -43,7 +48,7 @@ const props = defineProps({
 const text = computed(() => {
   return props.message.text.replace(
     regexp,
-    "<a href='https://vk.com/$1' target='_blank' style='color: #2a5885;font-weight: 500'>$2</a>"
+    '<a href=\'https://vk.com/$1\' target=\'_blank\' style=\'color: #2a5885;font-weight: 500\'>$2</a>'
   );
 });
 </script>
